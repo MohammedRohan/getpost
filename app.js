231b29be2aebb3286e7b5b407ef0f19c8d0d2e61
MohammedRohan <2806040@gmail.com>
2022-02-15 22:25:59 +0530

    update
    ss

--- app.js
+++ app.js
@@ -3,21 +3,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var app = new express()
 const cors = require("cors");
-app.use(cors({
-    origin:"*"
-}));
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
 
     res.send('hello')
 
 })
 
-app.post("/post",(req,res)=>{
+app.post("/post",async(req,res)=>{
 console.log("enter")
-    var demo = {
-        name: req.body
-    }
-var demo1 = register(demo)
-demo1.save()
+    var demo = new register({
+        name: req.body.name
+    })
+// var demo1 = register(demo)
+try{
+    const a1 =  await demo.save() 
+    res.json(a1)
+}catch(err){
+    res.send('Error')
+}
 })
+app.get('/data', async(req,res) => {
+    try{
+           const aliens = await register.find()
+           res.json(aliens)
+    }catch(err){
+        res.send('Error ' + err)
+    }
+})
+
 app.listen(process.env.PORT || 2222)
